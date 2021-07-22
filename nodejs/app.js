03508267f3063f00124227a261aacb08bb46c386
Canhui Huang <huangcanhui121@gmail.com>
2021-07-22 01:52:38 -0600

    s

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,44 @@
 "use strict";
+const http = require("http");
 const fs = require("fs");
 
-fs.writeFileSync("hello.txt", "Hello World hehe from Node.js");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
 
-console.log("hola bb");
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>oi yoi yoi</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-x = 6;
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFile("message.txt", message, (err) => {
+        res.writeHead(302, {});
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>oi yoi yoi</title></head>");
+  res.write("<body>soy guapo</body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
